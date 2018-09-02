9983344f75fa106880b761fd5bf19b951e8d5de3
Max Milton <max@wearegenki.com>
2018-09-02 13:06:35 +1000

    Update externs

--- component-externs.js
+++ component-externs.js
@@ -6,9 +6,3 @@
 // Svelte quoted method references
 var i = () => {};
 var m = () => {};
-// var state = {};
-// var update = () => {};
-// var destroy = () => {};
-// var intro = () => {};
-// var outro = () => {};
-// var start = () => {};
